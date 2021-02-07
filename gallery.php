1842f5efac971c265d137c28cdefe8f50036882a
Rahul <rkb9878@gmail.com>
2021-02-07 19:13:01 +0530

    added news

--- gallery.php
+++ gallery.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gallery | AUCT</title>
-    <?php include 'headerfile.php'?>
+    <?php include 'headerfile.php' ?>
 </head>
 <body>
 <?php include "navbar.php"; ?>
@@ -17,75 +17,120 @@
 <div class="container mt-4 mb-5">
     <div class="card-wrapper">
         <div class="box-shadow1">
-            <a href="media/gallery/new_2.jpeg" data-lightbox="image-1"><img src="media/gallery/new_2.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/new_2.jpeg" data-lightbox="image-1"><img src="media/gallery/new_2.jpeg"
+                                                                            class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/new_3.jpeg" data-lightbox="image-1"><img src="media/gallery/new_3.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/new_3.jpeg" data-lightbox="image-1"><img src="media/gallery/new_3.jpeg"
+                                                                            class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/new_4.jpeg" data-lightbox="image-1"><img src="media/gallery/new_4.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/new_4.jpeg" data-lightbox="image-1"><img src="media/gallery/new_4.jpeg"
+                                                                            class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/new_5.jpeg" data-lightbox="image-1"><img src="media/gallery/new_5.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/new_5.jpeg" data-lightbox="image-1"><img src="media/gallery/new_5.jpeg"
+                                                                            class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/2nd.jpeg" data-lightbox="image-1"><img src="media/gallery/2nd.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/2nd.jpeg" data-lightbox="image-1"><img src="media/gallery/2nd.jpeg"
+                                                                          class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/3.jpeg" data-lightbox="image-1"><img src="media/gallery/3.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/3.jpeg" data-lightbox="image-1"><img src="media/gallery/3.jpeg" class="gallery-image"
+                                                                        alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/5.jpeg" data-lightbox="image-1"><img src="media/gallery/5.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/5.jpeg" data-lightbox="image-1"><img src="media/gallery/5.jpeg" class="gallery-image"
+                                                                        alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/6.jpeg" data-lightbox="image-1"><img src="media/gallery/6.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/6.jpeg" data-lightbox="image-1"><img src="media/gallery/6.jpeg" class="gallery-image"
+                                                                        alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/7.jpeg" data-lightbox="image-1"><img src="media/gallery/7.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/7.jpeg" data-lightbox="image-1"><img src="media/gallery/7.jpeg" class="gallery-image"
+                                                                        alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/8.jpeg" data-lightbox="image-1"><img src="media/gallery/8.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/8.jpeg" data-lightbox="image-1"><img src="media/gallery/8.jpeg" class="gallery-image"
+                                                                        alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/9.jpeg" data-lightbox="image-1"><img src="media/gallery/9.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/9.jpeg" data-lightbox="image-1"><img src="media/gallery/9.jpeg" class="gallery-image"
+                                                                        alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/10.jpeg" data-lightbox="image-1"><img src="media/gallery/10.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/10.jpeg" data-lightbox="image-1"><img src="media/gallery/10.jpeg"
+                                                                         class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/11.jpeg" data-lightbox="image-1"><img src="media/gallery/11.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/11.jpeg" data-lightbox="image-1"><img src="media/gallery/11.jpeg"
+                                                                         class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/12.jpeg" data-lightbox="image-1"><img src="media/gallery/12.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/12.jpeg" data-lightbox="image-1"><img src="media/gallery/12.jpeg"
+                                                                         class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/13.jpeg" data-lightbox="image-1"><img src="media/gallery/13.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/13.jpeg" data-lightbox="image-1"><img src="media/gallery/13.jpeg"
+                                                                         class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/14.jpeg" data-lightbox="image-1"><img src="media/gallery/14.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/14.jpeg" data-lightbox="image-1"><img src="media/gallery/14.jpeg"
+                                                                         class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/15.jpeg" data-lightbox="image-1"><img src="media/gallery/15.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/15.jpeg" data-lightbox="image-1"><img src="media/gallery/15.jpeg"
+                                                                         class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/16.jpeg" data-lightbox="image-1"><img src="media/gallery/16.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/16.jpeg" data-lightbox="image-1"><img src="media/gallery/16.jpeg"
+                                                                         class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/17.jpeg" data-lightbox="image-1"><img src="media/gallery/17.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/17.jpeg" data-lightbox="image-1"><img src="media/gallery/17.jpeg"
+                                                                         class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/18.jpeg" data-lightbox="image-1"><img src="media/gallery/18.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/18.jpeg" data-lightbox="image-1"><img src="media/gallery/18.jpeg"
+                                                                         class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/19.jpeg" data-lightbox="image-1"><img src="media/gallery/19.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/19.jpeg" data-lightbox="image-1"><img src="media/gallery/19.jpeg"
+                                                                         class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/20.jpeg" data-lightbox="image-1"><img src="media/gallery/20.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/20.jpeg" data-lightbox="image-1"><img src="media/gallery/20.jpeg"
+                                                                         class="gallery-image" alt=""></a>
         </div>
         <div class="box-shadow1">
-            <a href="media/gallery/new_1.jpeg" data-lightbox="image-1"><img src="media/gallery/new_1.jpeg" class="gallery-image" alt=""></a>
+            <a href="media/gallery/new_1.jpeg" data-lightbox="image-1"><img src="media/gallery/new_1.jpeg"
+                                                                            class="gallery-image" alt=""></a>
         </div>
-
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="box-shadow1">
+                <div id="fb-root"></div>
+                <script async defer crossorigin="anonymous"
+                        src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v9.0&appId=2962298483849043&autoLogAppEvents=1"
+                        nonce="PGoZCguj"></script>
+                <div class="fb-video" data-href="https://fb.watch/3vDgiCkciN/" data-width="500" data-show-text="false">
+                    <blockquote cite="https://developers.facebook.com/LudhianaLive/videos/456087398909925/"
+                                class="fb-xfbml-parse-ignore"><a
+                                href="https://developers.facebook.com/LudhianaLive/videos/456087398909925/">ਸਿੱਖਿਆ
+                            ਵਿਰੋਧੀ
+                            ਨੀਤੀਆਂ ਤੋਂ ਪ੍ਰੇਸ਼ਾਨ ਟੀਚਰ</a>
+                        <p>ਸਿੱਖਿਆ ਵਿਰੋਧੀ ਨੀਤੀਆਂ ਤੋਂ ਪ੍ਰੇਸ਼ਾਨ ਟੀਚਰ</p>Posted by <a
+                                href="https://www.facebook.com/LudhianaLive/">Ludhiana LIVE</a> on Sunday, 24 January
+                        2021
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6"></div>
     </div>
 </div>
 
